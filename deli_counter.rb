c6418fdbf14087a82d44947c27e04c080bedd9a0
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-09-26 18:48:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,6 +7,9 @@ def line(katz_deli)
     puts "The line is currently empty."
   else 
     currently = "The line is currently:"
+    katz_deli.each do |element| 
+      currently << 
+    end 
   end 
 end   
 
