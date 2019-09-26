9c1edede1afcc9fa6829d83a283ab589232e35cb
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-09-26 18:44:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+require 'pry'
+katz_deli = []
+counter = 0 
+
+def line(katz_deli)
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  else 
+    puts "The line is currently: "
+  end 
+end   
+
